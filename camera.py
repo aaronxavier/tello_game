c2507921e4dc85d506178e6db92b910a5dd2ada6
Aaron Xavier (FLW) <aaron.xavier@tu-dortmund.de>
2025-05-27 15:09:39 +0200

    Add bbox for detections

--- camera.py
+++ camera.py
@@ -63,6 +63,15 @@ class Camera:
             frame = cv2.resize(frame, (self.frame_w, self.frame_h))
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             data, bbox, _ = self.detector.detectAndDecode(frame)
+        
+        if bbox is not None:
+            # Draw green box around detection
+            pts = bbox.astype(int).reshape((-1, 1, 2))
+            cv2.polylines(frame, [pts], isClosed=True, color=(0, 255, 0), thickness=2)
+    
+        else:
+            bbox = []
+
         if data:
             self.current_detection = data
             if data not in self.detections:
